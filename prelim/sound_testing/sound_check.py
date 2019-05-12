658049fca322886bbedc07893206f17c6eb61e63
Agatha Uy <yuahtaga@gmail.com>
2019-05-13 01:03:40 +0800

    AirDrums_v2.py integrated code from 2sticks_w_vel_and_dir.py
    Not yet with Stick class

--- prelim/sound_testing/sound_check.py
+++ prelim/sound_testing/sound_check.py
@@ -1,11 +1,12 @@
 import pygame
 import time
+#         self.directory_sound = "../../sounds/"
 
 pygame.mixer.pre_init()
 pygame.init()
 pygame.mixer.init(frequency=22050,size=-16,channels=2,buffer=4096)
-snare = pygame.mixer.Sound('sounds/snare.wav')
-hihat = pygame.mixer.Sound('sounds/hihat.wav')
+snare = pygame.mixer.Sound('../../sounds/snare.wav')
+hihat = pygame.mixer.Sound('../../sounds/hihat.wav')
 for i in range(5):
     snare.play()
     time.sleep(0.5)
